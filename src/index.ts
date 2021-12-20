c1589758cdb1ce84bb7c4179b34de9a8f3bb1e2e
Kevin Frei <kevinfrei@hotmail.com>
2021-12-19 17:21:42 -0800

    Added a couple exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 export * as Comms from './comms';
-export { Persistence } from './persist';
+export * from './persist';
 export * as Shell from './shell';
 export * from './win';
